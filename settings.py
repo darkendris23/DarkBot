febfde8fda3f8810c71a4fa1a3597abe82777457
darkendris23 <42562055+darkendris23@users.noreply.github.com>
2020-02-05 20:05:27 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,18 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+ 
+# The prefix that will be used to parse commands.
+# It doesn't have to be a single character!
+COMMAND_PREFIX = "!"
+ 
+# The bot token. Keep this secret!
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+ 
+# The now playing game. Set this to anything false-y ("", None) to disable it
+NOW_PLAYING = COMMAND_PREFIX + "commands"
+ 
+# Base directory. Feel free to use it if you want.
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
